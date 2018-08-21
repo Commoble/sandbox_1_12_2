621d49b9a4a76a95753d1abef535062c6fb2ceac
Commoble <josephbettendorff@yahoo.com>
2018-08-21 15:51:01 -0500

    Added event bus subscriber to client event handler

--- src/main/java/com/github/commoble/sandbox/client/ClientEventHandler.java
+++ src/main/java/com/github/commoble/sandbox/client/ClientEventHandler.java
@@ -6,8 +6,10 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class ClientEventHandler
 {
 	@SubscribeEvent
