aa5c3b827662125a578d6ca74f21dc608c108751
Commoble <josephbettendorff@yahoo.com>
2019-01-10 15:48:13 -0600

    Battery block has facing

--- src/main/java/com/github/commoble/sandbox/common/block/BlockBattery.java
+++ src/main/java/com/github/commoble/sandbox/common/block/BlockBattery.java
@@ -7,12 +7,18 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.world.World;
 
-public class BlockBattery extends Block implements ITileEntityProvider
+public class BlockBattery extends BlockWithFacing implements ITileEntityProvider
 {
-
 	public BlockBattery()
 	{
 		super(Material.IRON);
@@ -20,6 +26,9 @@ public class BlockBattery extends Block implements ITileEntityProvider
 		this.setSoundType(SoundType.METAL);
 		this.setHardness(2.0F);
 		this.setResistance(5.0F);
+		
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.WEST));
+        
 	}
 
 	@Override
@@ -28,5 +37,4 @@ public class BlockBattery extends Block implements ITileEntityProvider
 		// TODO Auto-generated method stub
 		return new TileEntityBattery();
 	}
-
 }
---
+++ src/main/java/com/github/commoble/sandbox/common/electrical/ChargePacket.java
@@ -0,0 +1,6 @@
+package com.github.commoble.sandbox.common.electrical;
+
+public class ChargePacket
+{
+	public double charge = 0D;
+}
